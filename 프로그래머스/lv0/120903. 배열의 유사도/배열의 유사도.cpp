657947e2b69b9703942f87cd581afe72236caded
원치현 <58906858+chihyeonWON@users.noreply.github.com>
2023-03-08 22:50:17 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<string> s1, vector<string> s2) {
+    int answer = 0;
+    for(string i : s1) {
+        for(string j : s2) {
+            if(i == j) {
+                answer++;
+            }
+        }
+    } 
+    return answer;
+}
