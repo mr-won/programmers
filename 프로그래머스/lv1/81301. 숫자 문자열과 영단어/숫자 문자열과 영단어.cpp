527b2c6d20b612a457ca3f16464b1b41866da31c
원치현 <58906858+chihyeonWON@users.noreply.github.com>
2023-03-11 01:53:10 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int solution(string s) {
+    vector<string> patterns = { "zero","one","two","three","four","five","six","seven","eight","nine" };
+    string num = "0123456789";
+    for (int i = 0; i < num.size(); i++) 
+        s = regex_replace(s, regex(patterns[i]), num.substr(i,1));
+    return stoi(s);
+}
