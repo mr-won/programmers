4491b0abd1d28f2da600d9418af48f40235621ee
chihyeonwon <heungeob1003@gmail.com>
2024-07-18 02:06:19 +0900

    [level 0] Title: 문자 리스트를 문자열로 변환하기, Time: 25.29 ms, Memory: 64.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(arr: Array<String>): String {
+        var answer: String = ""
+        
+        answer = arr.joinToString("")
+        
+        return answer
+    }
+}
