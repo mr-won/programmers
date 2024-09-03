e6930e1bcc5ff2005e6f0ed237d5b4827dfafd35
chihyeonwon <heungeob1003@gmail.com>
2024-09-03 12:09:30 +0900

    [level 2] Title: 타겟 넘버, Time: 9.67 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,30 @@
+class Solution {
+    
+    int[] numbers;
+    int target;
+    int answer;
+    
+    void dfs (int index, int sum) {
+        // 탈출 조건
+        if(index == numbers.length) {
+            if(sum == target) {
+                answer++;
+            }
+            return;
+        }
+        
+        // 수행동작
+        dfs(index + 1, sum + numbers[index]); // 더한것
+        dfs(index + 1, sum - numbers[index]); // 뺀것
+    }
+    
+    public int solution(int[] numbers, int target) {
+        answer = 0;
+        this.numbers = numbers;
+        this.target = target;
+        
+        dfs(0, 0);
+        
+        return answer;
+    }
+}
